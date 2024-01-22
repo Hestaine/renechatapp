87600311da6fd5cf9e19a654a36b3f256a1656d0
hashkitti001 <iJeddy5.0@gmail.com>
2024-01-22 04:57:04 +0100

    Patch to address client side routing issue (Replaced BrowserRouter with createBrowserRouter)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,44 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
+// import App from './App.jsx'
 import './index.css'
+import App from './App';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Chat from './components/Chat';
+import Chatroom from './components/Chatrooms';
+import NotFound from './components/NotFound';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "login",
+    element: <Login/>
+  },
+  {
+    path: "signup",
+    element: <Signup/>
+  },
+  {
+    path: "chatroom/:id",
+    element: <Chat/>
+  },
+  {
+    path: "chatrooms",
+    element: <Chatroom/>
+  },
+  {
+    path: "*",
+    element: <NotFound/>
+  },
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+   <RouterProvider router={router}/>
   </>
 )
